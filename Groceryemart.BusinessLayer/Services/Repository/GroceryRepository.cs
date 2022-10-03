18a67e0c08bec7c9202c5a94666d8fdc6be2db89
sanahaju <sanahaju123@gmail.com>
2022-10-03 17:56:20 +0530

    Add project files.

---
+++ Groceryemart.BusinessLayer/Services/Repository/GroceryRepository.cs
@@ -0,0 +1,81 @@
+﻿using GroceryEmart.DataLayer;
+using GroceryEmart.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroceryEmart.BusinessLayer.Services.Repository
+{
+    public class GroceryRepository : IGroceryRepository
+    {
+        /// <summary>
+        /// Creating referance Variable of DbContext
+        /// </summary>
+        private readonly GroceryemartDbContext _groceryemartDbContext;
+        public GroceryRepository(GroceryemartDbContext groceryemartDbContext)
+        {
+            _groceryemartDbContext = groceryemartDbContext;
+        }
+        /// <summary>
+        /// Get All Poduct from Product table
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IEnumerable<Product>> GetAllProduct()
+        {
+            //do code here
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Get Product from Product table by Product Id
+        /// </summary>
+        /// <param name="ProductId"></param>
+        /// <returns></returns>
+        public async Task<Product> GetProductById(int ProductId)
+        {
+            //do code here
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Get List of Category from Category table
+        /// </summary>
+        /// <returns></returns>
+        public IList<Category> CategoryList()
+        {
+            //do code here
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Place Order and verify registred user
+        /// </summary>
+        /// <param name="ProductId"></param>
+        /// <param name="UserId"></param>
+        /// <returns></returns>
+        public async Task<bool> PlaceOrder(int ProductId, int UserId)
+        {
+            //do code here
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Get product by name from Product table
+        /// </summary>
+        /// <param name="ProductName"></param>
+        /// <returns></returns>
+        public async Task<IEnumerable<Product>> ProductByName(string ProductName)
+        {
+            //do code here
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Get product by categoryId from Product table
+        /// </summary>
+        /// <param name="CatId"></param>
+        /// <returns></returns>
+        public async Task<IEnumerable<Product>> GetProductByCategory(int CatId)
+        {
+            //do code here
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Groceryemart.BusinessLayer/Services/Repository/UserGroceryRepository.cs
@@ -0,0 +1,76 @@
+﻿using GroceryEmart.DataLayer;
+using GroceryEmart.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroceryEmart.BusinessLayer.Services.Repository
+{
+    public class UserGroceryRepository : IUserGroceryRepository
+    {
+        /// <summary>
+        /// Creating referance Variable of DbContext
+        /// </summary>
+        private readonly GroceryemartDbContext _groceryemartDbContext;
+        
+        /// <summary>
+        /// Injecting Referance variable in UserGroceryRepository class Constructor
+        /// </summary>
+        public UserGroceryRepository(GroceryemartDbContext groceryemartDbContext)
+        {
+            _groceryemartDbContext = groceryemartDbContext;
+        }
+        /// <summary>
+        /// Get User by Id from ApplicationUser table
+        /// </summary>
+        /// <param name="UserId"></param>
+        /// <returns></returns>
+        public async Task<ApplicationUser> GetUserById(int UserId)
+        {
+            //do code here
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Verify User using this method if its is registred or not
+        /// </summary>
+        /// <param name="Email"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public async Task<ApplicationUser> Login(string Email, string password)
+        {
+            //do code here
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Logout function
+        /// </summary>
+        /// <returns></returns>
+        public Task<bool> Logout()
+        {
+            //do code here
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Register new user in ApplicationUser table
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        public async Task<ApplicationUser> Register(ApplicationUser user)
+        {
+            //do code here
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Update Registred user in ApplicationUser table
+        /// </summary>
+        /// <param name="UserId"></param>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        public async Task<ApplicationUser> UpdateUser(int UserId, ApplicationUser user)
+        {
+            //do code here
+            throw new NotImplementedException();
+        }
+    }
+}
